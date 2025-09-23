840f4eb1782578646c703bd6f0bf00e27cf871ca
anyeloeduardoromerotorres-rgb <anyeloeduardoromerotorres@gmail.com>
2025-09-23 01:46:53 -0500

    funcionando en server y front trabajando

--- frontend/constants/api.ts
+++ frontend/constants/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// ⚠️ Cambia esta IP por la IP pública de tu servidor cuando lo subas
-const API_BASE_URL = "http://192.168.18.16:3000/api";
+// Backend público desplegado en el VPS
+const API_BASE_URL = "http://195.133.93.48:3000/api";
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
